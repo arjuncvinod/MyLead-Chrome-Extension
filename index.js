aafe7672007803c43af876e7063a08d79736f7ad
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-13 23:50:58 +0530

    list nested within a tag

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
   let listItems = "";
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += "<li>" + myLeads[i] + "</li>";
+    listItems +="<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
   }
   ulEl.innerHTML = listItems;
 }
