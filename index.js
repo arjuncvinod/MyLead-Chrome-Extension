6d94e66a22474cdf457d912f060c27faad4f1ff2
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-13 23:33:47 +0530

    list renderd

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let myLeads = [];
+let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
-   myLeads.push(inputEl.value);
-   console.log(myLeads);
+  myLeads.push(inputEl.value);
+  console.log(myLeads);
 });
+
+for (let i = 0; i < myLeads.length; i++) {
+  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+}
