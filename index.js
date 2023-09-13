e09286de775518dbcd52d35706148ba423d06a94
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-13 23:12:55 +0530

    js added

---
+++ index.js
@@ -0,0 +1,7 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+
+inputBtn.addEventListener("click", function () {
+  console.log("Button clicked!");
+});
