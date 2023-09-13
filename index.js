a99aa25b1d6000b3ff2761da45da4fdd4d269266
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-13 23:48:46 +0530

    input cleared

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ulEl = document.getElementById("ul-el");
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   renderLeads()
-  console.log(myLeads);
+  inputEl.value = "";
 });
 
 function renderLeads() {
