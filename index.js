8a29b25936e831f342ed7cc9c89c6813ab4ca13f
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-13 23:19:58 +0530

    input value stored

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function () {
-  console.log("Button clicked!");
+   myLeads.push(inputEl.value);
+   console.log(myLeads);
 });
