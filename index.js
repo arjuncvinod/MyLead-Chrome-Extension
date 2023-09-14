40ef3a0ef936155f2559dc82863b7cb172c897e9
Arjun C Vinod <arjuncvinod007@gmail.com>
2023-09-14 23:45:39 +0530

    list rendered from local storage

--- index.js
+++ index.js
@@ -3,24 +3,30 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+console.log(leadsFromLocalStorage);
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  renderLeads();
+}
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads()
   inputEl.value = "";
-
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  renderLeads();
 });
 
 function renderLeads() {
-  let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-  console.log(leadsFromLocalStorage);
   let listItems = "";
-  for (let i = 0; i < leadsFromLocalStorage.length; i++) {
-    listItems += `<li>
-                <a target='_blank' href='${leadsFromLocalStorage[i]}'>
-                    ${leadsFromLocalStorage[i]}
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
                 </a>
-            </li>`;
+            </li>
+        `;
   }
   ulEl.innerHTML = listItems;
 }
